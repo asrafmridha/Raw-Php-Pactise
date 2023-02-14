2947e372ba0e03780b8a7ec90a03f8091dcfc0d8
asraf-mridha <asrafmridha@gmail.com>
2023-02-14 21:45:20 +0600

    array_shift array_unshift array_pop array_push function use

--- array.php
+++ array.php
@@ -18,7 +18,28 @@ for ($i = 0; $i < count($students); $i++) {
     echo $students[$i] . PHP_EOL;
 }
 
+// $stdnt = array_shift($students);
+// $stdnt = array_pop($students);
+// array_push($students, "priynka");
+// array_unshift($students, 'Tahmina');
+
 //for show all data in a array by foreach
 foreach ($students as $student) {
     echo $student . PHP_EOL;
 }
+
+//  এ্যারে শিফট এ্যারের ফাস্ট ইলিমেন্ট বের করে রিটান করে দেয়
+// array_shift();
+// $stdnt=array_shift($students);
+
+// এ্যারে পপ এ্যারের লাস্ট ইলিমেন্ট বের করে রিটান করে দেয়
+// array_pop();
+// $stdnt = array_pop($students);
+
+// array_unshift() এ্যারের প্রথমে নতুন ডাটা এন্ট্রি করার জন্য
+// array_unshift();
+// array_unshift($students,'Tahmina');
+
+// array_push() এ্যারের শেষে নতুন ডাটা এন্ট্রি করার জন্য
+// array_push();
+// array_push($students,"priynka");
