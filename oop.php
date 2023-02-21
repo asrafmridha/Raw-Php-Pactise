0d540b1c504eaed4988230c88dbbc673ea150422
asraf-mridha <asrafmridha@gmail.com>
2023-02-21 18:54:30 +0600

    Constructor Method

--- oop.php
+++ oop.php
@@ -17,6 +17,34 @@ class Fruit
 }
 
 $fruit = new Fruit();
-$fruit->name = "Tahmina";  //Set Name
+$fruit->name = "Tahmina\n";  //Set Name
 // $fruit->set_name('Tahmina'); //Set Name with Parameter
 echo $fruit->get_name(); //Get Name
+
+class Human
+{
+  // Properties
+  public $name;
+  public $age;
+
+  function __construct($pesonName = '', $personAge = 0)
+  {
+    $this->name = $pesonName;
+    $this->age  = $personAge;
+  }
+  
+  // Methods
+  function SayIdentity()
+  {
+    if ($this->age) {
+      echo "Hi I am {$this->name} and I am {$this->age} years old\n";
+    } else {
+      echo "Hi I am {$this->name} and I don't know how old I am\n";
+    }
+  }
+}
+
+$human = new Human('Tahmina');
+$human2 = new  Human("Priyanka",23);
+$human->SayIdentity();
+$human2->SayIdentity();
