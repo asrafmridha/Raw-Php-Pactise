8279cba3eb7b0f46fc7b7311fb48099d0341565b
asraf-mridha <asrafmridha@gmail.com>
2023-02-23 15:16:32 +0600

    Interfaces oop

--- oop.php
+++ oop.php
@@ -102,15 +102,57 @@ $myClass->eat();
 
 abstract class newClass
 {
-   final function sayHi()
+  final function sayHi()
   {
-    echo "Doing Something";
+    echo "Doing Something\n";
   }
 }
 class newClass2 extends newClass
 {
-
 }
 
 $cc = new newClass2();
 $cc->sayHi();
+
+// Interface definition
+interface Animal
+{
+  public function makeSound();
+  
+}
+
+// Class definitions
+class Cat implements Animal
+{
+  public function makeSound()
+  {
+    echo " Meow ";
+  }
+}
+
+class Dog implements Animal
+{
+  public function makeSound()
+  {
+    echo " Bark ";
+  }
+}
+
+class Mouse implements Animal
+{
+  public function makeSound()
+  {
+    echo " Squeak ";
+  }
+}
+
+// Create a list of animals
+$cat = new Cat();
+$dog = new Dog();
+$mouse = new Mouse();
+$animals = array($cat, $dog, $mouse);
+
+// Tell the animals to make a sound
+foreach ($animals as $animal) {
+  $animal->makeSound();
+}
