b0c071b1ecc2e13b8cde8f61d792f15af28f431d
asraf-mridha <asrafmridha@gmail.com>
2023-02-24 15:41:13 +0600

    Private property or method canonly be accessed within the class

--- accessModifier.php
+++ accessModifier.php
@@ -10,6 +10,23 @@ class MyClass
     private $city   = "Pangsha";
 }
 $objClass = new MyClass();
-echo $objClass->name;
+echo $objClass->name.PHP_EOL;
 // echo $objClass->age;
 // echo $objClass->city;
+
+// private
+// The property or method can ONLY be accessed within the class
+class AddNumbers
+{
+    private $num1 = 25;
+    private $num2 = 15;
+
+    public function addTwo()
+    {
+        $number3 = $this->num1 + $this->num2;
+        echo $number3;
+    }
+}
+
+$objAddNumbers = new AddNumbers();
+$objAddNumbers->addTwo();
