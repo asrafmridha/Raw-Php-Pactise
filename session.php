62c13eb2f01540655a9b93adb1e6ec64611f51b1
asraf-mridha <asrafmridha@gmail.com>
2023-03-09 21:53:27 +0600

    Session Count ++

--- session.php
+++ session.php
@@ -2,7 +2,11 @@
 // Start the session
 session_start();
 
-    // Set session variables
-    $_SESSION["favcolor"] = "green";
-    $_SESSION["favanimal"] = "cat";
-    echo "Session variables are set";
+// Set session variables
+// $_SESSION["favcolor"] = "green";
+// $_SESSION["favanimal"] = "cat";
+// echo "Session variables are set";
+
+$_SESSION['count'] = $_SESSION['count'] ?? 1;
+$_SESSION['count']++;
+echo $_SESSION['count'];
