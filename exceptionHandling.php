49b616624f948376538e3e928fd80c26fa9ac19d
asraf-mridha <asrafmridha@gmail.com>
2023-03-14 21:06:02 +0600

    Show Code by exception

--- exceptionHandling.php
+++ exceptionHandling.php
@@ -8,13 +8,15 @@ class Student
     {
         $this->name = $name;
         if ($age < 4) {
-            throw new Exception('Invalid Age');
+            throw new Exception('Invalid Age',1001);
+        } elseif ($age > 35) {
+            throw new Exception('Too Old', 1002);
         }
         $this->age = $age;
     }
 }
 try {
-    $student = new Student('Tahmina', 0);
+    $student = new Student('Tahmina', 3);
 } catch (Exception $e) {
-   echo $e->getMessage();
+    echo $e->getCode() . ":" . $e->getMessage();
 }
