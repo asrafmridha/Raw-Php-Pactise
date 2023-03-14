1b488e44847ef19531f0786c6851ac26a7a31d07
asraf-mridha <asrafmridha@gmail.com>
2023-03-14 22:10:44 +0600

    Custom Exceptions

--- exceptionHandling.php
+++ exceptionHandling.php
@@ -8,7 +8,7 @@ class Student
     {
         $this->name = $name;
         if ($age < 4) {
-            throw new Exception('Invalid Age',1001);
+            throw new Exception('Invalid Age', 1001);
         } elseif ($age > 35) {
             throw new Exception('Too Old', 1002);
         }
@@ -18,5 +18,27 @@ class Student
 try {
     $student = new Student('Tahmina', 3);
 } catch (Exception $e) {
-    echo $e->getCode() . ":" . $e->getMessage();
+    echo $e->getCode() . ":" . $e->getMessage().PHP_EOL;
 }
+
+class MyException extends Exception
+{
+}
+class NetworkException extends Exception
+{
+}
+
+function testException(){
+    throw new NetworkException();
+    
+}
+
+try{
+    testException();
+}catch (MyException $e){
+    echo "Customize Exception";
+} catch (NetworkException $e) {
+    echo "Network Exception";
+}
+
+
