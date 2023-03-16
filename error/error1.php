be94ffa451be5289038dde8dc54f8c69f856adc2
asraf-mridha <asrafmridha@gmail.com>
2023-03-16 19:33:36 +0600

    Custom Eroor Message

--- error/error1.php
+++ error/error1.php
@@ -3,5 +3,12 @@
 error_reporting(E_ALL);
 
 // to off error message 
-error_reporting(0);
+// error_reporting(0);
 echo $data;
+
+function custom_error_message($errno, $errmessage, $errfile, $errline)
+{
+    echo "Error [{$errno}] : {$errmessage} in {$errfile} on line number {$errline}";
+}
+set_error_handler('custom_error_message');
+echo substr([1234], 2);
